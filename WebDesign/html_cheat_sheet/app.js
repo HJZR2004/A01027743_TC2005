4a8095d190fd90d36cfa614920f0768e987715e6
julianRamirez <A01027743@tec.mx>
2024-06-01 20:46:14 -0600

    Upload the html, and css activities

--- WebDesign/html_cheat_sheet/app.js
+++ WebDesign/html_cheat_sheet/app.js
@@ -5,13 +5,16 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+// Iniciando el servidor
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+  });
 
 // Configurando la ruta principal
-app.get('/', (req, res) => {
+app.get('/htmlBasics', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'html_cheat_sheet.html'));
 });
 
-// Iniciando el servidor
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
-});
+app.get('/cssBasics', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'css_cheat_sheet.html'));
+  });
